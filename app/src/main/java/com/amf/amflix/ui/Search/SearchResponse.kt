d57198941c0433097dac460dabab344e2a693225
AdrianMF99 <adrianmfer99@gmail.com>
2024-05-24 22:49:05 +0200

    anims

--- app/src/main/java/com/amf/amflix/ui/Search/SearchResponse.kt
+++ app/src/main/java/com/amf/amflix/ui/Search/SearchResponse.kt
@@ -1,7 +1,12 @@
 package com.amf.amflix.ui.Search
 
 import com.amf.amflix.retrofit.models.movies.Movie
+import com.amf.amflix.retrofit.models.series.TVSeries
 
 data class SearchResponse(
     val results: List<Movie>
+)
+
+data class SearchResponsetv(
+    val results: List<TVSeries>
 )
--- app/src/main/java/com/amf/amflix/ui/Search/SearchViewModel.kt
+++ app/src/main/java/com/amf/amflix/ui/Search/SearchViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.amf.amflix.common.Constants
 import com.amf.amflix.retrofit.models.movies.Movie
+import com.amf.amflix.retrofit.models.series.TVSeries
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -13,24 +14,32 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class SearchViewModel : ViewModel() {
 
-    private val _searchResults = MutableLiveData<List<Movie>>()
-    val searchResults: LiveData<List<Movie>> = _searchResults
+    private val _searchResults = MutableLiveData<MutableList<Movie>>()
+    val searchResults: LiveData<MutableList<Movie>> = _searchResults
 
-    fun search(query: String) {
-        // Aquí harías la llamada a tu API para buscar las películas y series
-        // Ejemplo de llamada usando Retrofit:
+    private val _searchResultstv = MutableLiveData<MutableList<TVSeries>>()
+    val searchResultstv: LiveData<MutableList<TVSeries>> = _searchResultstv
+
+    init {
+        _searchResults.value = mutableListOf()
+        _searchResultstv.value = mutableListOf()
+    }
+
+    fun search(query: String, page: Int = 1) {
         val retrofit = Retrofit.Builder()
             .baseUrl(Constants.BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
         val api = retrofit.create(ApiService::class.java)
-        val call = api.searchMoviesAndSeries(Constants.API_KEY, query)
+        val call = api.searchMoviesAndSeries(Constants.API_KEY, query, page)
 
         call.enqueue(object : Callback<SearchResponse> {
             override fun onResponse(call: Call<SearchResponse>, response: Response<SearchResponse>) {
                 if (response.isSuccessful) {
-                    _searchResults.postValue(response.body()?.results)
+                    val mediaItems = response.body()?.results ?: emptyList()
+                    _searchResults.value?.addAll(mediaItems)
+                    _searchResults.postValue(_searchResults.value)
                 }
             }
 
@@ -38,5 +47,34 @@ class SearchViewModel : ViewModel() {
                 // Manejar el error
             }
         })
+    }
+
+    fun searchtv(query: String, page: Int = 1) {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val api = retrofit.create(ApiService::class.java)
+        val call = api.searchSeries(Constants.API_KEY, query, page)
+
+        call.enqueue(object : Callback<SearchResponsetv> {
+            override fun onResponse(call: Call<SearchResponsetv>, response: Response<SearchResponsetv>) {
+                if (response.isSuccessful) {
+                    val mediaItems = response.body()?.results ?: emptyList()
+                    _searchResultstv.value?.addAll(mediaItems)
+                    _searchResultstv.postValue(_searchResultstv.value)
+                }
+            }
+
+            override fun onFailure(call: Call<SearchResponsetv>, t: Throwable) {
+                // Manejar el error
+            }
+        })
+    }
+
+    fun clearSearchResults() {
+        _searchResults.value?.clear()
+        _searchResultstv.value?.clear()
     }
 }
